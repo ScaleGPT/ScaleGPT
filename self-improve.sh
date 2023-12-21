a552f5dcd62cc41172a12025255693eb782775f6
Your Name <you@example.com>
2023-12-21 20:52:15 +0000

    Add prompts and files for software development frameworks

--- self-improve.sh
+++ self-improve.sh
@@ -6,3 +6,11 @@ echo "$PROMPT_DIRECTORIES"
 
 PROMPT_FRAMEWORKS="Suggests comprehensive frameworks for scaling software development framework across large organizations"
 echo "$PROMPT_FRAMEWORKS"
+
+EXISTS_SOFTWARE_DEVELOPMENT_FRAMEWORKS="$(ls Software_Development_Frameworks)"
+for FRAMEWORK in $EXISTS_SOFTWARE_DEVELOPMENT_FRAMEWORKS
+do
+    PROMPT_ROLES="List roles of $FRAMEWORK"
+    echo "$PROMPT_ROLES" > "Software_Development_Frameworks/$FRAMEWORK/PROMPT_ROLES.md"
+    touch "Software_Development_Frameworks/$FRAMEWORK/ROLES.md"
+done
