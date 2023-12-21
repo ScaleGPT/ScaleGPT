45871162295a9fbf493249cf992d02671fafecea
Your Name <you@example.com>
2023-12-21 22:02:29 +0000

    Refactor create_project.sh script to handle existing project and framework
    This commit refactors the create_project.sh script to handle the case where a project or framework already exists. It checks for the existence of the project and framework files before creating new ones. If they exist, it displays an error message and prompts the user to remove them before creating a new project or framework. This improves the user experience and prevents accidental overwriting of existing projects or frameworks.

--- create_project.sh
+++ create_project.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-mkdir data
-echo "I am creating a new project. We should define following things:"
-read -p "What is the project? " PROJECT
-echo "$PROJECT" > data/PROJECT.md
+DATA_PROJECT_PATH="$(pwd)/data/PROJECT.md"
+DATA_PROJECT_EXISTS="$(ls data/PROJECT.md)"
+if [ -n "$DATA_PROJECT_EXISTS" ]; then
+    echo "Project exists, please remove it before creating a new one"
+else
+    mkdir data
+    echo "I am creating a new project. We should define following things:"
+    read -p "What is the project? " PROJECT
+    echo "$PROJECT" >data/PROJECT.md
+fi
+DATA_PROJECT="$(cat $DATA_PROJECT_PATH)"
+DATA_FRAMEWORK_PATH="$(pwd)/data/FRAMEWORK.md"
+DATA_FRAMEWORK_EXISTS="$(ls data/FRAMEWORK.md)"
+if [ -n "$DATA_FRAMEWORK_EXISTS" ]; then
+    echo "Framework exists, please remove it before creating a new one"
+else
+    PROMPT="I am creating a new project. $DATA_PROJECT. I am going to choose a framework for this project from the following list: $(ls Software_Development_Frameworks). Which one do you want to choose?"
+    echo "$PROMPT"
+    echo
+    read -p "Which one do you want to choose? " FRAMEWORK
+    echo "$FRAMEWORK" >$DATA_FRAMEWORK_PATH
+fi
+DATA_FRAMEWORK="$(cat $DATA_FRAMEWORK_PATH)"
+PROMPT="I am creating a new project. $DATA_PROJECT. The framework for this project is $DATA_FRAMEWORK. Our project is going to have following roles: $(ls Software_Development_Frameworks/$DATA_FRAMEWORK/Group_of_Roles). Which one goes first?"
+echo "$PROMPT"
