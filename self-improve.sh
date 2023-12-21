ade3497d9baa73719b540c6429f30a9ecb677348
Your Name <you@example.com>
2023-12-21 21:15:21 +0000

    Add group of roles and list roles in each group

--- self-improve.sh
+++ self-improve.sh
@@ -10,7 +10,14 @@ echo "$PROMPT_FRAMEWORKS"
 EXISTS_SOFTWARE_DEVELOPMENT_FRAMEWORKS="$(ls Software_Development_Frameworks)"
 for FRAMEWORK in $EXISTS_SOFTWARE_DEVELOPMENT_FRAMEWORKS
 do
+    echo
     PROMPT_GROUP_OF_ROLES="List group of roles in $FRAMEWORK"
     echo "$PROMPT_GROUP_OF_ROLES" > "Software_Development_Frameworks/$FRAMEWORK/PROMPT_GROUP_OF_ROLES.md"
     touch "Software_Development_Frameworks/$FRAMEWORK/GROUP_ROLES.md"
+    mkdir "Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles"
+    GROUP_ROLES="$(ls Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles)"
+    # echo "$GROUP_ROLES"
+    for GROUP_ROLE in $GROUP_ROLES; do
+        echo "List roles in $GROUP_ROLE"
+    done
 done
