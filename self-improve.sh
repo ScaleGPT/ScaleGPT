4802677875f6342aca377b1c7086ab10daa0cbbb
Your Name <you@example.com>
2023-12-24 22:21:47 +0000

    Refactor directory structure and file paths

--- self-improve.sh
+++ self-improve.sh
@@ -14,18 +14,19 @@ do
     PROMPT_GROUP_OF_ROLES="List group of roles in $FRAMEWORK"
     echo "$PROMPT_GROUP_OF_ROLES" > "Software_Development_Frameworks/$FRAMEWORK/PROMPT_GROUP_OF_ROLES.md"
     touch "Software_Development_Frameworks/$FRAMEWORK/GROUP_ROLES.md"
-    mkdir "Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles"
+    GROUP_OF_ROLES_PATH="Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles"
+    mkdir "$GROUP_OF_ROLES_PATH"
     touch "Software_Development_Frameworks/$FRAMEWORK/Order_for_Group_of_Roles"
-    GROUP_ROLES="$(ls Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles)"
+    GROUP_ROLES="$(ls $GROUP_OF_ROLES_PATH)"
     # echo "$GROUP_ROLES"
     for GROUP_ROLE in $GROUP_ROLES; do
         echo "List roles in $GROUP_ROLE of $FRAMEWORK"
-        ROLES="$(ls Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles/$GROUP_ROLE)"
+        ROLES="$(ls $GROUP_OF_ROLES_PATH/$GROUP_ROLE)"
         for ROLE in $ROLES; do
             echo "$ROLE"
             PROMPT_ACTIVITIES="List activities in $ROLE of $GROUP_ROLE in $FRAMEWORK"
-            echo $PROMPT_ACTIVITIES > "Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles/$GROUP_ROLE/$ROLE/PROMPT_ACTIVITIES.md"
-            touch "Software_Development_Frameworks/$FRAMEWORK/Group_of_Roles/$GROUP_ROLE/$ROLE/ACTIVITIES.md"
+            echo $PROMPT_ACTIVITIES > "$GROUP_OF_ROLES_PATH/$GROUP_ROLE/$ROLE/PROMPT_ACTIVITIES.md"
+            touch "$GROUP_OF_ROLES_PATH/$GROUP_ROLE/$ROLE/ACTIVITIES.md"
         done
     done
 done
